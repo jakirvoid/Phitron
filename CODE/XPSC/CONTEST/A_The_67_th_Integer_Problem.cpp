081657f4f5c1584a01c946c821c2beb03fd33a90
JAKIR VOID <jakirvoid@gmail.com>
2026-04-05 20:23:08 +0600

    XPSC added

---
+++ CODE/XPSC/CONTEST/A_The_67_th_Integer_Problem.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int x;
+        cin >> x;
+        if(x==67){
+            cout << x<<'\n';
+        }
+        else{
+            cout << x + 1 <<'\n';
+        }
+    }
+
+    return 0;
+}
---
+++ CODE/XPSC/CONTEST/B_The_67_th_6_7_Integer_Problem.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        vector<int> a(7);
+        for(int i = 0;i<7;i++){
+            cin >> a[i];
+        }
+        sort(a.begin(),a.end());
+        int sum = 0;
+
+        for(int i=0;i<6;i++){
+            a[i] *= -1;
+            sum += a[i]; 
+        }
+        cout << sum+ a[6] <<'\n';
+    }
+
+
+    return 0;
+}
